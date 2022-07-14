9ee74461d39d99482181c044fc52d98a0ce7d237
nagyorsolya <orsolya.n91@gmail.com>
2022-07-14 21:34:04 +0200

    Implemented drag and drop functionality but only for moving forward

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,64 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { DragDropContext, DropResult } from 'react-beautiful-dnd';
+import { useState, useEffect } from 'react';
 
 import Drivers from './components/Drivers';
+import { Driver } from './interfaces/DriverInterface';
 
 function App() {
+
+  const [drivers, setDrivers] = useState<Driver[]>([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3000/api/drivers')
+      .then(async (res) => {
+        const response = await res.json();
+        setDrivers(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  const overtake = (id: number) => {
+    fetch(`http://localhost:3000/api/drivers/${id}/overtake`, {
+        method: 'POST'
+    }).then(async (res) => {
+        const response = await res.json();
+        setDrivers(response);
+    })
+}
+
+  const onDragEnd = (result: DropResult) => {
+    const { destination, draggableId, source } = result;
+    if (!destination) {
+      return;
+    }
+    if (source.index === destination.index) {
+      return;
+    }
+    if (source.index < destination.index) {
+      //TODO: implement logic for dragging drive down the list
+      return;
+    } else {
+      fetch(`http://localhost:3000/api/drivers/${draggableId}/overtaketo/${destination.index + 1}`, {
+        method: 'POST'
+      })
+        .then(async (res) => {
+          const response = await res.json();
+          setDrivers(response);
+        })
+    }
+  }
+
   return (
-    <BrowserRouter >
-      <Routes>
-        <Route path="/drivers" element={<Drivers />} />
-      </Routes>
-    </BrowserRouter>
+    <DragDropContext onDragEnd={onDragEnd}>
+      <BrowserRouter >
+        <Routes>
+          <Route path="/drivers" element={<Drivers drivers={drivers} overtake={overtake}/>} />
+        </Routes>
+      </BrowserRouter>
+    </DragDropContext>
   );
 }
 
--- frontend/src/components/Drivers.tsx
+++ frontend/src/components/Drivers.tsx
@@ -1,49 +1,42 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
+import { Draggable, Droppable } from 'react-beautiful-dnd';
+
 
 import { Driver } from '../interfaces/DriverInterface';
 
-const Drivers: React.FC = () => {
-
-    const [drivers, setDrivers] = useState([]);
-
-    useEffect(() => {
-        fetch('http://localhost:3000/api/drivers')
-            .then(async (res) => {
-                const response = await res.json();
-                setDrivers(response);
-            })
-            .catch((error) => {
-                console.log(error);
-            });
-    }, []);
-
-    const overTake = (id: number) => {
-        fetch(`http://localhost:3000/api/drivers/${id}/overtake`, {
-            method: 'POST'
-        }).then(async (res) => {
-            const response = await res.json();
-            setDrivers(response);
-        })
-    }
-
-    const driverList = drivers.length ? drivers.map((driver: Driver) => {
+const Drivers: React.FC<{ drivers: Driver[], overtake: (id: number) => any }> = (props) => {
+    const driverList = props.drivers.length ? props.drivers.map((driver: Driver, index) => {
         return (
-            <li className="collection-item avatar" key={driver.id}>
-                <img alt="The driver" className="circle" src={`http://localhost:3000/${driver.imgUrl}`} />
-                <b><span className="title">{`${driver.firstname} ${driver.lastname}`}</span></b>
-                <p>{`Team: ${driver.team}`} <br />
-                    {`Current place: ${driver.place}`} <br />
-                    {`Code: ${driver.code}`}
-                </p>
-                {driver.place === 1 ? null : <button className="waves-effect waves-light btn-small secondary-content" onClick={() => overTake(driver.id)}>Overtake</button>}
-            </li>
+            <Draggable draggableId={driver.id.toString()} index={index} key={driver.id}>
+                {
+                    (provided) => (
+                        <li className="collection-item avatar" key={driver.id} {...provided.draggableProps} {...provided.dragHandleProps} ref={provided.innerRef}>
+                            <img alt="The driver" className="circle" src={`http://localhost:3000/${driver.imgUrl}`} />
+                            <b><span className="title">{`${driver.firstname} ${driver.lastname}`}</span></b>
+                            <p>{`Team: ${driver.team}`} <br />
+                                {`Current place: ${driver.place}`} <br />
+                                {`Code: ${driver.code}`}
+                            </p>
+                            {driver.place === 1 ? null : <button className="waves-effect waves-light btn-small secondary-content" onClick={() => props.overtake(driver.id)}>Overtake</button>}
+                        </li>
+                    )
+                }
+            </Draggable>
         )
     }) : (<p>There are no drivers to show.</p>);
     return (
-        <ul className="collection">
-            {driverList}
-        </ul>
+        <Droppable droppableId="drivers">
+            {
+                (provided) => (
+                    <ul className="collection" ref={provided.innerRef} {...provided.droppableProps}>
+                        {driverList}
+                        {provided.placeholder}
+                    </ul>
+                )
+            }
+        </Droppable>
     );
 }
 
 export default Drivers;
+
